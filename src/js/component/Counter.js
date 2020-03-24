d148618b5072e92cf1cb70c6e7db5b207d7b9ad0
Nelson <nlparra.dev@gmail.com>
2020-03-24 02:02:51 +0000

    work on the code - need to fix counter

--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import propTypes from "prop-types";
 
-export function Counter(props) {
+export function Counter() {
+	const [one, setOne] = useState(0);
+	const [two, setTwo] = useState(0);
+	const [three, setThree] = useState(0);
+	const [four, setFour] = useState(0);
+
+	let counter = 0;
+
+	useEffect(() => {
+		setInterval(function() {
+			setFour(Math.floor((counter / 1000) % 10));
+			setThree(Math.floor(counter / 100) % 100);
+			setTwo(Math.floor(counter / 10) % 10);
+			setOne(Math.floor(counter / 1) % 1);
+			counter++;
+		}, 1000);
+	});
+
 	return (
 		<div className="container">
 			<div className="calendar">
 				<i className="far fa-clock" />
 			</div>
-			<div className="four"> {props.numberFour} </div>
-			<div className="three"> {props.numberThree} </div>
-			<div className="two"> {props.numberTwo} </div>
-			<div className="one"> {props.numberOne} </div>
+			<div className="four"> {four}</div>
+			<div className="three"> {three} </div>
+			<div className="two">{two}</div>
+			<div className="one"> {one} </div>
 		</div>
 	);
 }
-
-let counter = 0;
-
-setInterval(function() {
-	let four = Math.floor((counter / 1000) % 10);
-	let three = Math.floor((counter / 100) % 10);
-	let two = Math.floor((counter / 10) % 10);
-	let one = Math.floor((counter / 1) % 10);
-	console.log(four, three, two, one);
-	counter++;
-}, 1000);
-
-Counter.propTypes = {
-	numberFour: propTypes.number,
-	numberThree: propTypes.number,
-	numberTwo: propTypes.number,
-	numberOne: propTypes.number
-};
+// Counter.propTypes = {
+// 	numberFour: propTypes.number,
+// 	numberThree: propTypes.number,
+// 	numberTwo: propTypes.number,
+// 	numberOne: propTypes.number
+// };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,12 +2,5 @@ import React from "react";
 import { Counter } from "./Counter";
 
 export function Home() {
-	return (
-		<Counter
-			numberOne={one}
-			numberTwo={two}
-			numberThree={three}
-			numberFour={four}
-		/>
-	);
+	return <Counter />;
 }
